06dffaf0d3c4af96bd66129ddfb3cb7c875ec2c3
Glassous <143624737+Glassous@users.noreply.github.com>
2025-11-26 12:19:23 +0800

    修改版本号为1.2

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -32,8 +32,8 @@ android {
         applicationId = "com.glassous.fiagoods"
         minSdk = 33
         targetSdk = 36
-        versionCode = 2
-        versionName = "1.1"
+        versionCode = 3
+        versionName = "1.2"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
 
